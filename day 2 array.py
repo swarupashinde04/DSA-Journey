dc928a33974faaafeb79c4327441716ad1812ba3
Swarupa Shinde <swarupa8387@gmail.com>
2026-06-20 11:57:41 +0530

    Add files via upload

---
+++ day 2 array.py
@@ -0,0 +1,10 @@
+#index based traversal :
+arr = [1, 2, 3, 4, 5]
+for i in range(len(arr)):
+    print(i, arr[i])
+#output - 0 1, 1 2, 2 3, 3 4, 4 5
+
+#reverse an array :
+arr = [1, 2, 3, 4, 5]
+for i in range(len(arr)-1,-1,-1):
+    print(arr[i])
